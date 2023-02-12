3a2e5c9302d6a7ae85c4606ec3c46995bbbcdd2b
zeynepselinerkan <Q2MCSUGt7ZjRYex>
2023-02-13 02:51:03 +0300

    Reset Password Page was created.

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -97,7 +97,10 @@ namespace AspNetCoreIdentityApp.Web.Controllers
      
             return View();
         }
-
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ AspNetCoreIdentityApp.Web/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [EmailAddress(ErrorMessage = "Email is not in the correct format.")]
+        [Required(ErrorMessage = "Email field must be filled.")]
+        [Display(Name = "Email :")]
+        public string EmailAddress { get; set; }
+    }
+}
